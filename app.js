70941d815fbbb98cdcd1691ad4f74856f5825bf4
Juan Urdaneta <jurdanetac10@gmail.com>
2024-04-26 13:09:42 -0400

    Fix error

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('5') // change this string to ensure a new version deployed
+  res.send('6') // change this string to ensure a new version deployed
 })
 
 app.use(express.static('dist'))
