64e7f6f2d9175413094cee1405c224c4e0dc39a3
Juan Urdaneta <jurdanetac10@gmail.com>
2024-05-02 09:03:32 -0400

    Intentionally break lint to test failure notification

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const express = require('express')
-const app = express()
+const express = require('express'); const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
